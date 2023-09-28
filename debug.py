d66dcc512051c586bb3f8b160484233842ef59be
ZalZarak <peter.kopylov@posteo.eu>
2023-09-28 15:44:40 +0200

    config adjustment and scrolling for debug_length

--- debug.py
+++ debug.py
@@ -1,3 +1,5 @@
+import copy
+
 import cv2
 import tkinter as tk
 from tkinter import ttk
@@ -5,17 +7,20 @@ import numpy as np
 import statistics
 
 import pandas as pd
+import pyrealsense2 as rs
 
 import helper
 import main
+from config import config
+
 
 color_ranges = {
-    "upper blue": 146,
-    "upper green": 255,
-    "upper red": 100,
-    "lower blue": 0,
-    "lower green": 150,
-    "lower red": 0,
+    "upper blue": config["Main"]["color_range"][1][0],
+    "upper green": config["Main"]["color_range"][1][1],
+    "upper red": config["Main"]["color_range"][1][2],
+    "lower blue": config["Main"]["color_range"][0][0],
+    "lower green": config["Main"]["color_range"][0][1],
+    "lower red": config["Main"]["color_range"][0][2],
 }
 
 # Define the font settings
@@ -24,7 +29,7 @@ font_scale = 0.3
 font_thickness = 1
 font_color = (255, 255, 255)  # White color
 
-def debug_color_mask(resolution: tuple[int, int] = (480, 270), fps: int = 30, flip: int = 0, playback_file: str = None):
+def debug_color_mask():
     def update_slider(var_name):
         new_val = int(sliders_gui[var_name].get())
         values_gui[var_name].set(new_val)
@@ -63,11 +68,10 @@ def debug_color_mask(resolution: tuple[int, int] = (480, 270), fps: int = 30, fl
 
         sliders_gui[var_name].bind("<Motion>", lambda event, var_name=var_name: update_slider(var_name))
 
-    cl = main.RGBDto3DPose(playback=playback_file is not None, duration=-1, playback_file=playback_file,
-                           resolution=resolution, fps=fps, flip=flip, countdown=0, translation=None,
-                           savefile_prefix=None, save_joints=False, save_bag=False,
-                           show_rgb=False, show_depth=False, show_joints=False, show_color_mask=False,
-                           simulate_limbs=False, simulate_joints=False, simulate_joint_connections=False)
+    debug_config = copy.deepcopy(config["Main"])
+    debug_config["save_joints"] = debug_config["save_bag"] = debug_config["show_rgb"] = debug_config["show_depth"] = debug_config["show_joints"] \
+        = debug_config["show_color_mask"] = debug_config["simulate_limbs"] = debug_config["simulate_joints"] = debug_config["simulate_joint_connections"] = False
+    cl = main.RGBDto3DPose(**debug_config)
 
     cl.prepare()
 
@@ -93,15 +97,22 @@ def debug_color_mask(resolution: tuple[int, int] = (480, 270), fps: int = 30, fl
         cl.pipeline.stop()
 
 
-def debug_search_area(resolution: tuple[int, int] = (480, 270), fps: int = 30, flip: int = 0, playback_file: str = None):
+def debug_search_area():
     # mode 0: length, mode 1: depth, mode 2: search area
+
+    debug_config = copy.deepcopy(config["Main"])
+    debug_config["save_joints"] = debug_config["save_bag"] = debug_config["show_rgb"] = debug_config["show_depth"] \
+        = debug_config["show_color_mask"] = debug_config["simulate_limbs"] = debug_config["simulate_joints"] = debug_config["simulate_joint_connections"] = False
+    debug_config["show_joints"] = True
+    cl = main.RGBDto3DPose(**debug_config)
+    cl.prepare()
 
     def update_slider(var_name, value_idx):
         new_val = int(sliders_gui[var_name][value_idx].get())
         values_gui[var_name][value_idx].set(new_val)
 
         search_areas_hr[var_name][value_idx] = new_val
-        search_areas[main.joint_map[var_name]] = helper.generate_base_search_area(search_areas_hr[var_name][0], search_areas_hr[var_name][1])
+        search_areas[cl.joint_map[var_name]] = helper.generate_base_search_area(search_areas_hr[var_name][0], search_areas_hr[var_name][1])
 
     def update_value(var_name, value_idx, new_value):
         try:
@@ -110,7 +121,7 @@ def debug_search_area(resolution: tuple[int, int] = (480, 270), fps: int = 30, f
             sliders_gui[var_name][value_idx].set(new_val)
 
             search_areas_hr[var_name][value_idx] = new_val
-            search_areas[main.joint_map[var_name]] = helper.generate_base_search_area(search_areas_hr[var_name][0], search_areas_hr[var_name][1])
+            search_areas[cl.joint_map[var_name]] = helper.generate_base_search_area(search_areas_hr[var_name][0], search_areas_hr[var_name][1])
         except ValueError:
             pass
 
@@ -118,8 +129,8 @@ def debug_search_area(resolution: tuple[int, int] = (480, 270), fps: int = 30, f
     sliders_gui = {}
     entry_fields = {}
 
-    search_areas_hr = {k: list(v) for k, v in main.search_areas_hr.items()}
-    search_areas = {k: list(v) for k, v in main.search_areas.items()}
+    search_areas_hr = {k: list(v) for k, v in cl.search_areas_hr.items()}
+    search_areas = {k: list(v) for k, v in cl.search_areas.items()}
 
     gui = tk.Tk()
     gui.title("Search Areas")
@@ -167,15 +178,6 @@ def debug_search_area(resolution: tuple[int, int] = (480, 270), fps: int = 30, f
         sliders_gui[var_name][0].bind("<Motion>", lambda event, var_name=var_name, value_idx=0: update_slider(var_name, value_idx))
         sliders_gui[var_name][1].bind("<Motion>", lambda event, var_name=var_name, value_idx=1: update_slider(var_name, value_idx))
 
-
-    cl = main.RGBDto3DPose(playback=playback_file is not None, duration=-1, playback_file=playback_file,
-                           resolution=resolution, fps=fps, flip=flip, countdown=0, translation=None,
-                           savefile_prefix=None, save_joints=False, save_bag=False,
-                           show_rgb=False, show_depth=False, show_joints=True,
-                           simulate_limbs=False, simulate_joints=False, simulate_joint_connections=False)
-
-    cl.prepare()
-
     cv2.namedWindow("Debug-Search-Area")
 
     key = None
@@ -189,7 +191,7 @@ def debug_search_area(resolution: tuple[int, int] = (480, 270), fps: int = 30, f
                 color_image = color_image.copy()  # copy because cv2 cannot handle views of ndarrays e.g. if they were rotated etc.
 
                 center_x, center_y = joint2d
-                n = search_areas_hr[main.joint_map_rev[i]][0]*2 + 1
+                n = search_areas_hr[cl.joint_map_rev[i]][0]*2 + 1
                 top_left = (center_x - n // 2, center_y - n // 2)
                 bottom_right = (center_x + n // 2, center_y + n // 2)
                 cv2.rectangle(color_image, top_left, bottom_right, (64, 64, 64), 1)
@@ -199,7 +201,7 @@ def debug_search_area(resolution: tuple[int, int] = (480, 270), fps: int = 30, f
                 canvas.update_idletasks()
                 gui.update()
 
-            for pair in main.connections_list:
+            for pair in cl.connections:
                 point1, point2 = joints_2d[pair[0]], joints_2d[pair[1]]
                 if point1.any() != 0 and point2.any() != 0:
                     cv2.line(color_image, point1, point2, (128, 128, 128), 2)
@@ -212,30 +214,111 @@ def debug_search_area(resolution: tuple[int, int] = (480, 270), fps: int = 30, f
         cl.pipeline.stop()
 
 
-def debug_length(mode: int, output_filename: str = None, custom_connections=None, connections_except=None, resolution: tuple[int, int] = (480, 270), fps: int = 30, flip: int = 0,
-                 playback_file: str = None):
+def debug_length(mode: int, output_filename: str = None, custom_connections=None, connections_except=None):
     # mode 0: length, mode 1: depth
 
     assert mode in (0, 1), "mode is 0 or 1."
     assert connections_except is None or custom_connections is None, "Don't input connections_except and custom_connections at the same time"
 
-    cl = main.RGBDto3DPose(playback=playback_file is not None, duration=-1, playback_file=playback_file,
-                           resolution=resolution, fps=fps, flip=flip, countdown=0, translation=None,
-                           savefile_prefix=None, save_joints=False, save_bag=False,
-                           show_rgb=False, show_depth=False, show_joints=True,
-                           simulate_limbs=False, simulate_joints=False, simulate_joint_connections=False)
-
+    debug_config = copy.deepcopy(config["Main"])
+    debug_config["save_joints"] = debug_config["save_bag"] = debug_config["show_rgb"] = debug_config["show_depth"] \
+        = debug_config["show_color_mask"] = debug_config["simulate_limbs"] = debug_config["simulate_joints"] = debug_config[
+        "simulate_joint_connections"] = False
+    debug_config["show_joints"] = True
+    cl = main.RGBDto3DPose(**debug_config)
     cl.prepare()
+
+
+
+    def update_slider(var_name, value_idx):
+        new_val = int(sliders_gui[var_name][value_idx].get())
+        values_gui[var_name][value_idx].set(new_val)
+
+        if mode == 0:
+            cl.lengths_hr[var_name][value_idx] = new_val/100
+        else:
+            cl.depth_deviations_hr[var_name] = new_val/100 if new_val >= 0 else -1
+
+    def update_value(var_name, value_idx, new_value):
+        try:
+            new_val = int(new_value)
+            values_gui[var_name][value_idx].set(new_val)
+            sliders_gui[var_name][value_idx].set(new_val)
+
+            if mode == 0:
+                cl.lengths_hr[var_name][value_idx] = new_val / 100
+            else:
+                cl.depth_deviations_hr[var_name] = new_val / 100 if new_val >= 0 else -1
+        except ValueError:
+            pass
+
+    values_gui = {}
+    sliders_gui = {}
+    entry_fields = {}
+
+    gui = tk.Tk()
+    gui.title("Lengths" if mode == 0 else "Depths")
+
+    canvas = tk.Canvas(gui)
+    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+    scrollbar = ttk.Scrollbar(gui, orient=tk.VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+    frame = tk.Frame(canvas)
+    canvas.create_window((0, 0), window=frame, anchor="nw")
+
+    for var_name, value_range in cl.lengths_hr.items() if mode == 0 else cl.depth_deviations_hr.items():
+        if mode == 0:
+            values_gui[var_name] = [tk.IntVar(value=int(value_range[0]*100)), tk.IntVar(value=int(value_range[1]*100))]  # Create two IntVar instances for each variable
+        else:
+            values_gui[var_name] = [tk.IntVar(value=int(value_range*100 if value_range >= 0 else -1))]
+        inner_frame = tk.Frame(frame)
+        inner_frame.pack()
+
+        label = tk.Label(inner_frame, text=var_name)
+        label.pack(side=tk.LEFT)
+
+        if mode == 0:
+            sliders_gui[var_name] = [tk.Scale(inner_frame, from_=0, to=80, orient=tk.HORIZONTAL, variable=values_gui[var_name][0]),
+                                     tk.Scale(inner_frame, from_=0, to=80, orient=tk.HORIZONTAL, variable=values_gui[var_name][1])]
+        else:
+            sliders_gui[var_name] = [tk.Scale(inner_frame, from_=-1, to=40, orient=tk.HORIZONTAL, variable=values_gui[var_name][0])]
+
+        sliders_gui[var_name][0].pack(side=tk.LEFT)
+        if mode == 0:
+            sliders_gui[var_name][1].pack(side=tk.LEFT)
+
+        entry_frame = tk.Frame(inner_frame)
+        entry_frame.pack(side=tk.LEFT)
+
+        for value_idx in range(1 if mode == 1 else 2):
+            entry = tk.Entry(entry_frame, width=5)
+            entry.pack(side=tk.LEFT)
+            entry.bind("<Return>",
+                       lambda event, var_name=var_name, value_idx=value_idx, entry=entry: update_value(var_name, value_idx, entry.get()))
+            entry.bind("<FocusOut>",
+                       lambda event, var_name=var_name, value_idx=value_idx, entry=entry: update_value(var_name, value_idx, entry.get()))
+
+            entry_fields[(var_name, value_idx)] = entry
+
+        # Bind slider updates to the slider movement
+        sliders_gui[var_name][0].bind("<Motion>", lambda event, var_name=var_name, value_idx=0: update_slider(var_name, value_idx))
+        if mode == 0:
+            sliders_gui[var_name][1].bind("<Motion>", lambda event, var_name=var_name, value_idx=1: update_slider(var_name, value_idx))
 
     cv2.namedWindow("Debug-Length")
 
     lengths = {}
     if mode == 0:
-        lengths = {k: [] for k in main.lengths_hr.keys()}
+        lengths = {k: [] for k in cl.lengths_hr.keys()}
     elif mode == 1:
-        lengths = {k: [] for k in main.depth_deviations_hr.keys()}
+        lengths = {k: [] for k in cl.depth_deviations_hr.keys()}
 
-    connections = main.connections_list
+    connections = cl.connections
     if custom_connections is not None:
         connections = custom_connections
     elif connections_except is not None:
@@ -248,7 +331,18 @@ def debug_length(mode: int, output_filename: str = None, custom_connections=None
             joints_2d, confidences, joint_image = cl.openpose_handler.push_frame(color_image)
             joints_2d = joints_2d.astype(int)
 
-            joints_3d = cl.get_3d_coords(joints_2d, depth_frame)
+            joints_3d = np.zeros([joints_2d.shape[0], 3])
+            # get 3d coordinates for all joints
+            for i, (x, y) in enumerate(joints_2d):
+                try:
+                    depth = depth_frame.get_distance(x, y)
+                    if depth > 0:
+                        # get 3d coordinates and reorder them from y,x,z to x,y,z
+                        joints_3d[i] = rs.rs2_deproject_pixel_to_point(intrin=cl.intrinsics, pixel=(x, y), depth=depth)
+                    else:
+                        joints_3d[i] = 0
+                except RuntimeError:  # joint outside of picture
+                    pass
 
             for i, (joint2d, joint3d) in enumerate(zip(joints_2d, joints_3d)):
                 if joint3d[2] != 0:
@@ -257,16 +351,18 @@ def debug_length(mode: int, output_filename: str = None, custom_connections=None
             for pair in connections:
                 point1, point2 = joints_2d[pair[0]], joints_2d[pair[1]]
                 point1_3d, point2_3d = joints_3d[pair[0]], joints_3d[pair[1]]
-                lengths_key = main.joint_map_rev[pair[0]] + "-" + main.joint_map_rev[pair[1]]
+                lengths_key = cl.joint_map_rev[pair[0]] + "-" + cl.joint_map_rev[pair[1]]
 
                 if point1.any() != 0 and point2.any() != 0 and point1_3d[2] != 0 and point2_3d[2] != 0:
                     center_x = (point1[0] + point2[0]) // 2
                     center_y = (point1[1] + point2[1]) // 2
                     if mode == 0:
                         l = np.linalg.norm(point1_3d - point2_3d)
+                        valid = cl.lengths_hr[lengths_key][0] <= l <= cl.lengths_hr[lengths_key][1]
                     else:
                         l = abs(point1_3d[2] - point2_3d[2])
-                    color = (128, 128, 128) if main.lengths_hr[lengths_key][0] <= l <= main.lengths_hr[lengths_key][1] else (0, 0, 200)
+                        valid = (cl.depth_deviations_hr[lengths_key] < 0 or l <= cl.depth_deviations_hr[lengths_key])
+                    color = (128, 128, 128) if valid else (0, 0, 200)
                     lengths[lengths_key].append(l)
                     text = str(l.round(2))
 
@@ -277,6 +373,8 @@ def debug_length(mode: int, output_filename: str = None, custom_connections=None
                     cv2.putText(color_image, text, (text_x, text_y), font, font_scale, font_color, font_thickness)
 
             cv2.imshow("Debug-Joint", color_image)
+
+            gui.update()
 
             key = cv2.waitKey(1)
     finally:
@@ -362,4 +460,4 @@ def debug_length(mode: int, output_filename: str = None, custom_connections=None
 
 
 if __name__ == '__main__':
-    debug_color_mask()
+    debug_length(1)
